6111156aed8252fcdfb86d20cb00da6b7eb0e2f2
DerFrZocker <derrieple@gmail.com>
2023-11-07 16:33:49 +0100

    Get jersey running

---
+++ src/main/java/de/we2/am/therealone/web/RestConfig.java
@@ -0,0 +1,13 @@
+package de.we2.am.therealone.web;
+
+import de.we2.am.therealone.web.rest.Test;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestConfig extends ResourceConfig {
+
+    public RestConfig() {
+        register(Test.class);
+    }
+}
