0d41bc7af986bc35f6d2ec7866b8cc1505218f88
DerFrZocker <derrieple@gmail.com>
2023-11-07 10:11:19 +0100

    Add spring boot from https://start.spring.io/

---
+++ src/main/java/de/we2/am/therealone/TherealoneApplication.java
@@ -0,0 +1,13 @@
+package de.we2.am.therealone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherealoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherealoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/we2/am/therealone/TherealoneApplicationTests.java
@@ -0,0 +1,13 @@
+package de.we2.am.therealone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherealoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
