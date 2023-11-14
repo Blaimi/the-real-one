b68941e8ddc39b95117d9809d8c110aab7718811
DerFrZocker <derrieple@gmail.com>
2023-11-14 16:56:02 +0100

    Add server port and jersey application path

--- src/main/java/de/we2/am/therealone/web/RestConfig.java
+++ src/main/java/de/we2/am/therealone/web/RestConfig.java
@@ -1,12 +1,10 @@
 package de.we2.am.therealone.web;
 
 import de.we2.am.therealone.web.rest.Test;
-import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ApplicationPath("api/v2/assets")
 public class RestConfig extends ResourceConfig {
 
     public RestConfig() {
