8550210d4e0c3e3cb0ab581c93208a4979357a14
DerFrZocker <derrieple@gmail.com>
2023-11-18 15:52:00 +0100

    Add Room to

---
+++ src/main/java/de/we2/am/therealone/web/to/room/RoomTO.java
@@ -0,0 +1,11 @@
+package de.we2.am.therealone.web.to.room;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record RoomTO(UUID id, String name, @JsonProperty("storey_id") UUID storeyId, @JsonProperty("deleted_at") Instant deletedAt) {
+}
---
+++ src/main/java/de/we2/am/therealone/web/to/room/RoomsTO.java
@@ -0,0 +1,6 @@
+package de.we2.am.therealone.web.to.room;
+
+import java.util.List;
+
+public record RoomsTO(List<RoomTO> rooms) {
+}
