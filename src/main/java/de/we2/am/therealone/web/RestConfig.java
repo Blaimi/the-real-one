ceb9a9f57a5202937176d04b76d27475d0dc0434
DerFrZocker <derrieple@gmail.com>
2023-11-18 15:30:09 +0100

    Remove test endpoint

--- src/main/java/de/we2/am/therealone/web/RestConfig.java
+++ src/main/java/de/we2/am/therealone/web/RestConfig.java
@@ -1,7 +1,6 @@
 package de.we2.am.therealone.web;
 
 import de.we2.am.therealone.web.resource.StatusResource;
-import de.we2.am.therealone.web.rest.Test;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +9,5 @@ public class RestConfig extends ResourceConfig {
 
     public RestConfig() {
         register(StatusResource.class);
-        register(Test.class);
     }
 }
