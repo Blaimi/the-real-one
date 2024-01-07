4e924b2a709047785af5e25a6bb12d316a56c763
DerFrZocker <git@derfrzocker.de>
2024-01-07 13:18:14 +0100

    Allays throw InvalidArgumentException, when uuid cannot be parsed and also add uuid regex

--- src/main/java/de/we2/am/therealone/util/ConverterUtil.java
+++ src/main/java/de/we2/am/therealone/util/ConverterUtil.java
@@ -1,20 +1,20 @@
 package de.we2.am.therealone.util;
 
 import de.we2.am.therealone.exception.InvalidArgumentException;
-import de.we2.am.therealone.exception.NotFoundException;
 
-import java.util.Locale;
 import java.util.UUID;
 
 public final class ConverterUtil {
+
+    private static final String UUID_PATTERN = " ^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
 
     public static UUID convertId(String id, String objectType) {
         // It's easier to take a string as id and manually parse it (and throw an error if it is not an uuid)
         // Instead of taking an uuid and catch the thrown error from jakarta
         try {
             return UUID.fromString(id);
-        } catch (IllegalArgumentException e) {
-            throw new NotFoundException(String.format("Requested %s was not found", objectType.toLowerCase(Locale.ROOT)), String.format("A %s with the id '%s' does not exist", objectType.toLowerCase(Locale.ROOT), id), objectType, id);
+        } catch (Exception e) {
+            throw new InvalidArgumentException("Not a valid uuid", String.format("The id '%s' is not a valid uuid", id), objectType, "quarry parameter", id, UUID_PATTERN);
         }
     }
 
@@ -23,8 +23,8 @@ public final class ConverterUtil {
         // Instead of taking an uuid and catch the thrown error from jakarta
         try {
             return UUID.fromString(id);
-        } catch (IllegalArgumentException e) {
-            throw new InvalidArgumentException("Not a valid uuid", String.format("The id '%s' is not a valid uuid", id), objectType, argumentName, id, null);
+        } catch (Exception e) {
+            throw new InvalidArgumentException("Not a valid uuid", String.format("The id '%s' is not a valid uuid", id), objectType, argumentName, id, UUID_PATTERN);
         }
     }
 }
